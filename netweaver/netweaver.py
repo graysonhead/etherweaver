edde729d7ceb6535e1b43c6f34d9ddbae835712f
Grayson Head <grayson@graysonhead.net>
2018-06-14 01:07:20 -0500

    CLI argument parser

--- netweaver/netweaver.py
+++ netweaver/netweaver.py
@@ -1,2 +1,14 @@
+from netweaver.network_role import NetworkRole
+from netweaver.plugins.plugin_class_errors import *
+
+class CliApp:
+
+	def parse_pattern(self, pattern):
+		pass
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser(description='Netweaver is an application to orchestrate network configurations.')
+	parser.add_argument(type=str, name='selection-pattern', dest='selection-pattern')
 
 
