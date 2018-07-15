b4817cd42b329acf9dbb1564793d66c190127740
Grayson Head <grayson@graysonhead.net>
2018-07-15 13:23:37 -0500

    Fixing unit tests

--- netweaver/plugins/cumulus/test.py
+++ netweaver/plugins/cumulus/test.py
@@ -28,14 +28,12 @@ class TestPlugin(unittest.TestCase):
 	def test_hostname_case2(self):
 		dstate = {'hostname': 'newtest'}
 		cstate = {'hostname': 'test'}
-		self.plugin._hostname_push(dstate, cstate)
-		self.assertEqual(self.plugin.commands, ['net add hostname newtest'])
+		self.assertEqual(self.plugin._hostname_push(dstate, cstate), 'net add hostname newtest')
 
 	def test_hostname_case1(self):
 		dstate = {'hostname': 'test'}
 		cstate = {'hostname': 'test'}
-		self.plugin._hostname_push(dstate, cstate)
-		self.assertEqual(self.plugin.commands, [])
+		self.assertEqual(self.plugin._hostname_push(dstate, cstate), None)
 
 if __name__ == '__main__':
 	unittest.main()
